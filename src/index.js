8e0480f9cae4e88d28c5a50c1aba4b90cbc2d373
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-03 22:47:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+  fetchImages();
+  fetchBreeds();
+});
+
+function fetchImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+    .then((resp) => resp.json())
+    .then((data) => {
+      data.message.forEach((image) => addImage(image));
+    });
+}
+
+function addImage(dogImage) {
+  let dogContainer = document.querySelector("#dog-image-container");
+  let imgElement = document.createElement("img");
+  imgElement.src = dogImage;
+  dogContainer.appendChild(imgElement);
+}
+
+function fetchBreeds() {
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  fetch(breedUrl)
+    .then((resp) => resp.json())
+    .then((data) => {
+      breeds = Object.keys(data.message);
+    });
+}
+
+function addBreed(dogBreed) {
+  let breedUL = document.querySelector("#dog-breed");
+  let liElement = document.createElement("li");
+  liElement.value = dogBreed;
+  breedUL.appendChild(liElement);
+}
